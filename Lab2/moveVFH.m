b5403a74a6d0e25dc9e2fa752ba0cba7b59026f6
João Castiho <jpccastilho1999@gmail.com>
2022-03-17 00:48:23 +0000

    Changes

--- Lab2/moveVFH.m
+++ Lab2/moveVFH.m
@@ -6,10 +6,10 @@ if ( ~exist("tbot") )
     % Note: edit TurtleBot.m to define the robot and local host IP addresses
     tbot = TurtleBot(); 
 end 
-tbot.setPose(1,0.5,pi/2);
+tbot.setPose(3.5,3.5,pi/2);
 
 map = read_map("./maps/fmap_grid5.png");
 [x,y,theta] = tbot.readPose();
 plotPose(x,y,theta,map);
 
-activeArea = getActiveArea([x,y],map,20);
+active_cells = getActiveArea([x,y],map,30);
