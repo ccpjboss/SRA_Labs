bf7fac83ad3712e74c1b1a62eea99ad6f3056a0a
João Castiho <jpccastilho1999@gmail.com>
2022-03-17 00:47:20 +0000

    Added active area visualization

--- Lab2/getActiveArea.m
+++ Lab2/getActiveArea.m
@@ -11,5 +11,18 @@ low_limit_y = max(1,floor(robot_y_cell-N/2));
 high_limit_y = min(size(map,1),floor(robot_y_cell+N/2));
 
 activeArea = map(low_limit_y:high_limit_y,low_limit_x:high_limit_x);
+window_cm = N/2*5;
+x1=robotPose(1)-window_cm/100;
+y1=robotPose(2)-window_cm/100;
+x2=robotPose(1)+window_cm/100;
+y2=robotPose(2)+window_cm/100;
+
+sx = [x1, x2, x2, x1, x1];
+sy = [y1, y1, y2, y2, y1];
+
+figure(1)
+hold on 
+plot(sx, sy,'b--',"LineWidth",2)
+
 end
 
