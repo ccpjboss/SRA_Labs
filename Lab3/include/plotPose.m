1e42302d35eb6c48e2537309ca925d9777282004
João Castiho <jpccastilho1999@gmail.com>
2022-05-01 13:12:01 +0100

    Lab3 demo v4 update

--- Lab3/include/plotPose.m
+++ Lab3/include/plotPose.m
@@ -1,9 +1,8 @@
-function plotPose(x, y, theta,x_,y_,map, goalPose, n_points, nPose)
-    figure(1); clf; hold on; % clear figure, hold plots
-    plot(x, y,'--or', 'MarkerSize', 10)  % display (x,y) location of the robot
+function plotPose(x, y, theta,x_,y_,map, goalPose, n_points)
+
+    figure(1); 
     plot(x_(1:2:end), y_(1:2:end),'--')
-    quiver(x,y,cos(theta),sin(theta), 0.1, 'Color','r','LineWidth',1, 'ShowArrowHead',1)
-    quiver(x,y,cos(nPose(3)),sin(nPose(3)), 0.1, 'Color','b','LineWidth',1, 'ShowArrowHead',1)
+    drawTurtleBot(x,y,theta);
     quiver(0,0,1,0,'r')                 % draw arrow for x-axis 
     quiver(0,0,0,1,'g')                 % draw arrow for y-axis 
     axis([0, 4, 0, 4])                % the limits for the current axes [xmin xmax ymin ymax]
@@ -19,16 +18,9 @@ function plotPose(x, y, theta,x_,y_,map, goalPose, n_points, nPose)
             plot(goalPose(j,1),goalPose(j,2),'bx', 'MarkerSize', 5); %display locations of points
         end
     end
-
-
-    for i = 2:79
-        for j = 2:79
-            if map(i,j) > 0.7
-                %scatter(j/20,i/20,50,"black","filled")
-                plot(j./20,i./20, '--', 'LineWidth', 2, 'Color', 'black', 'Marker','.')
-            end
-        end
-    end
+    
+    [r,c] = find(map>0.75);
+    scatter(c./20,r./20,[],[0 0 0],'Marker','.')
 
 end
 
--- Lab3/moveVFH_LIDAR.m
+++ Lab3/moveVFH_LIDAR.m
@@ -11,7 +11,8 @@ end
 map = zeros(80);
 map(:) = 0.5;
 
-n_points = input('How many points?');
+answer = inputdlg('How many points?');
+n_points = str2double(answer{1});
 figure(1);
 title(('How many points?'))
 figure(1);
@@ -31,7 +32,6 @@ for j=1:n_points
         plot(goalPose(j,1),goalPose(j,2),'bx', 'MarkerSize', 5); %display locations of points
     end
 end
-
 
 tbot.setPose(0.5,0.5,pi/2);
 % goal_pose = [3.5,3.5];
@@ -53,34 +53,48 @@ y_ = [];
 h_smooth = [];
 
 l0 = zeros(80);
-l0(1,:) = 0.2;
-l0(end,:) = 0.2;
-l0(:,1) = 0.2;
-l0(:,end) = 0.2;
+% l0(1,:) = 0.2;
+% l0(end,:) = 0.2;
+% l0(:,1) = 0.2;
+% l0(:,end) = 0.2;
 
+figure(2); 
+map_handle = imshow(zeros(80));
+v = 0;
 for j=1:n_points
     dist = sqrt((goalPose(j,1)-x)^2+(goalPose(j,2)-y)^2);
     while (dist>0.15)
-        plotPose(x,y,theta,x_,y_,map,goalPose,n_points, nPose);
+        plotPose(x,y,theta,x_,y_,map,goalPose,n_points);
 
-        [scan, xydata, angles] = tbot.readLidar();
-        [x,y,theta] = tbot.readPose();
+        [scan, lddata, ldtimestamp] = tbot.readLidar();
+        [x,y,theta,timestamp] = tbot.readPose();
+        % compute the time diference between the pose reading and laser scan (in seconds) 
+        timeDiff =  timestamp - ldtimestamp;
+    
+        % Get in range lidar data indexs (valid data - obstacles).
+        [vidx] = tbot.getInRangeLidarDataIdx(lddata);
+        % load valid X/Y cartesian lidar readings          
+        xydata = lddata.Cartesian(vidx,:);
+    
+        % Get out of range lidar data indexs (too near or too far readings).
+        [nidx] = tbot.getOutRangeLidarDataIdx(lddata);
+        % load obstacle free orientations (angles)  
+        freeAng = lddata.Angles(nidx);
+
         [robotX, robotY] = world2grid(x,y,80);
 
-        transformMatrix = [cos(theta) -sin(theta) x-0.0305;
-    			           sin(theta) cos(theta) y;
+        transformMatrix = [cos(theta) -sin(theta) x-0.0305-(v*timeDiff*cos(theta));
+    			           sin(theta) cos(theta) y-(v*timeDiff*sin(theta));
 			               0 0 1];
 
         xyWorld = transformMatrix*[xydata';ones(1,size(xydata,1))];
         xyWorld = xyWorld(1:2,:);
         [xCell, yCell] = world2grid(xyWorld(1,:),xyWorld(2,:),80);
-        map = updateMap(map,xCell,yCell,[robotX, robotY],l0);
+        map = updateMap(map,xCell,yCell,[robotX, robotY,theta],l0,freeAng, transformMatrix);
         active_cells = getActiveArea([x,y],map,xCell', yCell',robotX, robotY,window_size);
         [world_x, world_y] = grid2world(active_cells(:,1),active_cells(:,2),size(map,1));
     
-        h_smooth = getPolarHistogram(world_y, y, world_x, x, theta, alpha,window_size);
-        drawHistogram(h_smooth);
-    
+        h_smooth = getPolarHistogram(world_y, y, world_x, x, theta, alpha,window_size);    
         [go_theta_deg] = getSteeringDirection(h_smooth, goalPose(j,:), x, y, alpha, theta);
         
         go_theta_rad = deg2rad(go_theta_deg(1));
@@ -89,27 +103,15 @@ for j=1:n_points
         nPose(1) = x + 0.3*cos(go_theta_rad);
         nPose(2) = y + 0.3*sin(go_theta_rad);
 
-        figure(3);
-        subplot(2,2,1);
-        rosPlot(scan);
-        subplot(2,2,2);
-        plot(xyWorld(1,:)',xyWorld(2,:)');
-        title('Laser Scan in World Frame')
-        axis([0 4 0 4]);
-        grid minor;
-        subplot(2,2,3);
-        plot(xCell,yCell);
-        title('Discrete map')
-        axis([0 80 0 80]);
-        grid minor;
-        subplot(2,2,4);
-        imshow(rot90(imcomplement(map)),[])
+        figure(2);
+        map_handle.CData = flip(map,1); %imshow(flip(map,1),[])
         title('map')
     
         figure(1);
         hold on;
         plot(nPose(1),nPose(2),'ro');
         hold off
+
         ek = sqrt((nPose(1)-x)^2+(nPose(2)-y)^2) - dk;
         integral = ((ek+ek_anterior)/2)*toc(delta_t); 
         delta_t = tic;
@@ -120,11 +122,17 @@ for j=1:n_points
         
         tbot.setVelocity(v,w);
     
-        [x,y,theta] = tbot.readPose();
         x_ = [x_ x];
         y_ = [y_ y];
         dist = sqrt((goalPose(j,1)-x)^2+(goalPose(j,2)-y)^2);
     end
 end
-
+figure(2);
+subplot(1,2,1)
+imshow(flip(map,1));
+subplot(1,2,2)
+colormap('gray')
+surface(1:80,1:80,map);
+axis([1 80 1 80]);
+title('Final Probabilistic Map')
 tbot.stop();
